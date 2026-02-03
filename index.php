c17ae346d71f1c6fe9a18a803178fe3abe457ed3
westras <r.westholm@telia.com>
2026-02-03 14:26:34 +0100

    kan ha gjort lite fel, tehe

--- index.php
+++ index.php
@@ -9,27 +9,12 @@
     <h1>Formulär med GET och POST</h1>
     
     <?php
-    // Hantera POST-data
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $postData = $_POST;
     } else {
         $postData = array();
     }
-    
-    $getData = $_GET;
     ?>
-
-    <h2>GET formulär</h2>
-    <form method="GET"> 
-        <label for="get_name">Namn:</label>
-        <input type="text" id="get_name" name="name" value="<?php echo htmlspecialchars($getData['name'] ?? ''); ?>">
-        
-        <label for="get_email">E-mail:</label>
-        <input type="email" id="get_email" name="email" value="<?php echo htmlspecialchars($getData['email'] ?? ''); ?>">
-        
-        <button type="submit">Skicka get</button>
-    </form>
-
 
     <h2>POST formulär</h2> 
     <form method="POST">
@@ -39,25 +24,18 @@
         <label for="post_email">E-mail:</label>
         <input type="email" id="post_email" name="email" value="<?php echo htmlspecialchars($postData['email'] ?? ''); ?>">
         
-        <button type="submit">Skicka post</button>
+        <button type="submit">Skicka</button>
     </form>
 
     <?php if (!empty($postData)): ?>
-        <h2>Data ifrån post formuläret</h2>
-        <ul>
-            <?php foreach ($postData as $key => $value): ?>
-                <li><strong><?php echo htmlspecialchars($key); ?>:</strong> <?php echo htmlspecialchars($value); ?></li>
-            <?php endforeach; ?>
-        </ul>
-    <?php endif; ?>
-
-    <?php if (!empty($getData)): ?>
-        <h2>Data ifrån get formuläret</h2>
-        <ul>
-            <?php foreach ($getData as $key => $value): ?>
-                <li><strong><?php echo htmlspecialchars($key); ?>:</strong> <?php echo htmlspecialchars($value); ?></li>
-            <?php endforeach; ?>
-        </ul>
+        <h2>GET formulär (med skickad data)</h2>
+        <form method="GET"> 
+            <label for="get_name">Namn:</label>
+            <input type="text" id="get_name" name="name" value="<?php echo htmlspecialchars($postData['name'] ?? ''); ?>" readonly>
+            
+            <label for="get_email">E-mail:</label>
+            <input type="email" id="get_email" name="email" value="<?php echo htmlspecialchars($postData['email'] ?? ''); ?>" readonly>
+        </form>
     <?php endif; ?>
 </body>
 </html>
